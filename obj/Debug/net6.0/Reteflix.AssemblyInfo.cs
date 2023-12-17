16f15ca670ab24759429e03f954f9cf30329a895
Moses <76195668+moscoade@users.noreply.github.com>
2023-12-17 23:53:02 +0000

    first

--- obj/Debug/net6.0/Reteflix.AssemblyInfo.cs
+++ obj/Debug/net6.0/Reteflix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reteflix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93d39c4d90d2c2b7a6f2040b1b19a3fc02da7a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+868117d7c516cf1ecf293a3a1403bb53b60e2f7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reteflix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reteflix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
